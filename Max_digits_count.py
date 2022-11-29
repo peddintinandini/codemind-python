a58002439c461286bfad97255ced19d4412f582d
peddintinandini <96322585+peddintinandini@users.noreply.github.com>
2022-11-29 20:15:12 +0530

    def con(n):     c=0     while n:         r=n%10         c+=1         n=n//10     return c n=int(input()) k=list(map(int,input().split())) c=0 e=0 for i in range(n):     if con(k[i])>c:         c=con(k[i]) for i in range(n):     if con(k[i])==c:         e+=1 print(e)

---
+++ Max_digits_count.py
@@ -0,0 +1,18 @@
+def con(n):
+    c=0
+    while n:
+        r=n%10
+        c+=1
+        n=n//10
+    return c
+n=int(input())
+k=list(map(int,input().split()))
+c=0
+e=0
+for i in range(n):
+    if con(k[i])>c:
+        c=con(k[i])
+for i in range(n):
+    if con(k[i])==c:
+        e+=1
+print(e)
