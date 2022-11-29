cc9de1c99a63f77a48ca94d4ccc7868862cab26b
peddintinandini <96322585+peddintinandini@users.noreply.github.com>
2022-11-29 19:44:29 +0530

    n,m=map(int,input().split()) k=list(map(int,input().split())) b=list(map(int,input().split())) c=[] d=[] for i in k:     for j in b:         if i not in c:            if i==j:               c.append(i) print(len(c))

---
+++ Count_elements_I.py
@@ -0,0 +1,11 @@
+n,m=map(int,input().split())
+k=list(map(int,input().split()))
+b=list(map(int,input().split()))
+c=[]
+d=[]
+for i in k:
+    for j in b:
+        if i not in c:
+           if i==j:
+              c.append(i)
+print(len(c))
