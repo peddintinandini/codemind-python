d1e97c36de0857a316059d6caf036792c9579716
peddintinandini <96322585+peddintinandini@users.noreply.github.com>
2022-12-05 20:38:11 +0530

    n=int(input()) d=list(map(int,input().split())) a,b=map(int,input().split()) m=[] for i in d:     if a>i:         m.append(i)     if b<i:         m.append(i) if len(m)==0:     print("-1") else:     print(min(m))

---
+++ minimum_not_between_A_and_B.py
@@ -0,0 +1,13 @@
+n=int(input())
+d=list(map(int,input().split()))
+a,b=map(int,input().split())
+m=[]
+for i in d:
+    if a>i:
+        m.append(i)
+    if b<i:
+        m.append(i)
+if len(m)==0:
+    print("-1")
+else:
+    print(min(m))
