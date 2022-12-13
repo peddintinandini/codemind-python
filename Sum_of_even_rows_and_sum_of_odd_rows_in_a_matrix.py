269291277863663a6a87902f45ed1cbe0a6334dc
peddintinandini <96322585+peddintinandini@users.noreply.github.com>
2022-12-13 14:38:42 +0530

    n,m=map(int,input().split()) mat=[[int(i) for i in input().split()] for j in range(n)] d=[] f=[] g=[] s=[] for i in range(0,len(mat)):     if i%2:         d.append(mat[i])     else:         f.append(mat[i]) for i in d:     for j in i:         g.append(j) for i in f:     for j in i:         s.append(j) print(sum(s),sum(g))

--- Sum_of_even_rows_and_sum_of_odd_rows_in_a_matrix.py
+++ Sum_of_even_rows_and_sum_of_odd_rows_in_a_matrix.py
@@ -2,8 +2,8 @@ n,m=map(int,input().split())
 mat=[[int(i) for i in input().split()] for j in range(n)]
 d=[]
 f=[]
-g=0
-s=0
+g=[]
+s=[]
 for i in range(0,len(mat)):
     if i%2:
         d.append(mat[i])
@@ -11,9 +11,9 @@ for i in range(0,len(mat)):
         f.append(mat[i])
 for i in d:
     for j in i:
-        g+=j
+        g.append(j)
 for i in f:
     for j in i:
-        s+=j
-print(s,g)
+        s.append(j)
+print(sum(s),sum(g))
     
